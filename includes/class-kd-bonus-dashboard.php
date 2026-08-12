3a35b8c1dec9587e8719908f7b85fd59dd209454
mantelimustikka-prog <mantelimustikka@gmail.com>
2026-08-12 23:11:29 +0100

    Revert "Fix dashboard progress bar: CSS stripped by wp_kses_post, rendering as visible text"

--- includes/class-kd-bonus-dashboard.php
+++ includes/class-kd-bonus-dashboard.php
@@ -97,8 +97,7 @@ class KD_Bonus_Dashboard {
 	 * Render the customer dashboard on the My Account endpoint.
 	 */
 	public function render_my_account_endpoint() {
-		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- shortcode output is sanitized internally.
-		echo $this->render_shortcode();
+		echo wp_kses_post( $this->render_shortcode() );
 	}
 
 	/**
